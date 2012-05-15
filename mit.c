ee48a92ff60fda81d95606e0139d437b41995d62
Mattias Johansson <mattias.johansson@atex.com>
2012-05-15 16:46:07 +0200

    Basic usage

--- mit.c
+++ mit.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Print usage and exit
+void usage();
 
 int main(int num_args, char** args) {
 
   if (num_args < 2) {
-    printf("Usage: mit <command>\n");
-  } else {
-    printf("Output of: %s\n", args[1]);
+    usage();
   }
 
   return 0;
+}
+
+void usage() {
+  printf("Usage: mit <command>\n");
+  exit(0);
 }
